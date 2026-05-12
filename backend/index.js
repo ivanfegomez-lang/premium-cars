d99f03078427df0b0f5046ac8e8cce3eea118828
Ivan Felipe Gomez Montaña <ivanfegomez@gmail.com>
2026-05-12 00:42:14 -0500

    deploy frontend production build

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,37 @@
 require("dotenv").config({ path: "./.env" });
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
+
 const vehiculosRoutes = require("./routes/vehiculos");
 const usuariosRoutes = require("./routes/usuarios");
 const reservasRoutes = require("./routes/reservas");
-
 
 const app = express();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+
 app.use("/reservas", reservasRoutes);
-
-// conexion a mongodb
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB conectado"))
-    .catch(err => console.log("error:", err));
-
-// ruta de prueba
-app.get("/", (req, res) => {
-    res.send("Api Funcionando")
-});
-
 app.use("/vehiculos", vehiculosRoutes);
 app.use("/usuarios", usuariosRoutes);
 
-//  levantar server
-app.listen(process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+// conexion mongodb
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+  .catch(err => console.log("error:", err));
+
+// servir frontend Angular
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// levantar server
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
